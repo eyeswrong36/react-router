6b358834e34e29221607ca86ba53bbb3c1c81307
unknown <jason.matarong@mobilemoney.ph>
2021-08-24 22:13:08 +0800

    re-arrange code

--- src/App.js
+++ src/App.js
@@ -81,31 +81,34 @@ function App() {
 
   return (
     <Router>
-    <div className="container">
-      <Header 
-        onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask} 
-      />
-      <Route 
-        path='/'
-        exact
-        render={(props) => (
-          <>
-            { showAddTask && <AddTask onAdd={addTask}  /> }
-            { tasks.length > 0 ? 
-              <Tasks 
-                tasks={tasks}
-                onDelete={deleteTask}
-                onToggle={toggleReminder} 
-              /> : 
-              'No Task To Show'
-            }
-          </>
-        )}
-      />
-      <Route path='/about' component={About} />
-      <Footer />
-    </div>
+      <div className="container">
+        <Header 
+          onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask} 
+        />
+        <Route 
+          path='/'
+          exact
+          render={(props) => (
+            <>
+              { showAddTask && <AddTask onAdd={addTask}  /> }
+              { tasks.length > 0 ? 
+                <Tasks 
+                  tasks={tasks}
+                  onDelete={deleteTask}
+                  onToggle={toggleReminder} 
+                /> : 
+                'No Task To Show'
+              }
+            </>
+          )}
+        />
+        <Route 
+          path='/about' 
+          component={About} 
+        />
+        <Footer />
+      </div>
     </Router>
   );
 }
